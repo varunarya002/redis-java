3c8c1c5d083d48efba741162209db015935272f5
Varun Arya <varun.arya002@gmail.com>
2025-01-23 02:20:54 +0530

    codecrafters submit [skip ci]

--- src/main/java/model/protocol/BulkStringProtocol.java
+++ src/main/java/model/protocol/BulkStringProtocol.java
@@ -6,15 +6,6 @@ public class BulkStringProtocol extends BaseProtocol<String>
   @Override
   public String serialize(String payload)
   {
-    String[] strings = payload.split(",");
-    int stringLength = strings.length;
-
-    StringBuilder stringBuilder = new StringBuilder(CRLF);
-
-    for (String s: strings) {
-      stringBuilder.append(s);
-      stringBuilder.append(CRLF);
-    }
-    return "$" + stringLength + stringBuilder;
+    return "$" + payload.length() + CRLF + payload + CRLF;
   }
 }
